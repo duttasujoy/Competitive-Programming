7be67cb6c06dd40375963a56b0d051b6f66aa6ad
Sujoy Dutta <sujoydutta1994@gmail.com>
2020-06-21 08:54:10 +0530

    Added code PermutationSequence leetcode challenge

---
+++ src/com/practice/leetcode/challenge/june/PermutationSequence.java
@@ -0,0 +1,9 @@
+package com.practice.leetcode.challenge.june;
+public class PermutationSequence {
+    public String getPermutation(int n, int k) {
+        String result ="";
+        
+        
+		return result;
+    }
+}
