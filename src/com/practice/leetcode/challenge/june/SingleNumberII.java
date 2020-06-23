a88314905e5d431cf9cb6c28ebc2f509442a0c57
Sujoy Dutta <sujoydutta1994@gmail.com>
2020-06-23 07:55:39 +0530

    Added code for SingleNumberII leetcode Challenge

---
+++ src/com/practice/leetcode/challenge/june/SingleNumberII.java
@@ -0,0 +1,15 @@
+package com.practice.leetcode.challenge.june;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumberII {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int single = nums[0];
+        for(int i:nums){
+//            if()
+        }
+		return single;
+    }
+}
