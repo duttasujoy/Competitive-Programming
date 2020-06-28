af74f5e8803a6a0c943e56da79133b78d4623af9
Sujoy Dutta <sujoydutta1994@gmail.com>
2020-06-28 08:56:06 +0530

    Added code for Perfect Squares leetcode challenge

---
+++ src/com/practice/leetcode/challenge/june/PerfectSquares.java
@@ -0,0 +1,14 @@
+package com.practice.leetcode.challenge.june;
+public class PerfectSquares {
+    public int numSquares(int n) {
+        
+        int dp[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+            dp[i]=i;
+            for(int j=2;j*j<=i;j++){
+                dp[i]=Math.min(dp[i],dp[i-j*j]+1);
+            }
+        }
+        return dp[n];
+    }
+}
