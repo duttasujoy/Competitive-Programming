c908e4b9eaf9310d37a8dcdb170a34d61d79b7f7
Sujoy Dutta <dutta.suoy@siemens.com>
2020-07-08 08:39:14 +0530

    Added code for HammingDistance leetcode challenge

---
+++ src/com/practice/leetcode/challenge/july/HammingDistance.java
@@ -0,0 +1,21 @@
+package com.practice.leetcode.challenge.july;
+public class HammingDistance {
+	public static void main(String[] args) {
+		HammingDistance obj = new HammingDistance();
+		int n1 =4;
+		int n2 =1;
+		System.out.println(obj.hammingDistance(n1 , n2 ));
+	}
+    public int hammingDistance(int n1, int n2) {
+        int x = n1 ^ n2; 
+        int setBits = 0; 
+
+        while (x > 0)  
+        { 
+            setBits += x & 1; 
+            x >>= 1; 
+        } 
+
+        return setBits; 
+    }
+}
